0cce17782ca0082e489f44d24f2248bed685e7dc
Nakahotty <naskuuu123@gmail.com>
2021-10-20 18:57:11 +0300

    Cleaned tasks

--- week_03/tasks.cpp
+++ week_03/tasks.cpp
@@ -1,23 +1,5 @@
 #include <iostream>
 
 int main() {
-    double bmi, w, h;
-    std::cout << "Enter weight: ";
-    std::cin >> w;
-
-    std::cout << "Enter height: ";
-    std::cin >> h;
-
-    bmi = w / (h*h);
-    std::cout << "Your BMI is " << bmi << std::endl;
-
-    if (bmi > 25) {
-        std::cout << "Overweight" << std::endl;
-    } else if (bmi < 25 && bmi > 18.5) {
-        std::cout << "Optimal" << std::endl;
-    } else {
-        std::cout << "Underweight" << std::endl;
-    }
-
-    return 0;
+    
 }
