bf605cdae0690fba94f77e73d27d55fb8e263466
Nakahotty <naskuuu123@gmail.com>
2021-11-19 12:11:00 +0200

    Update tasks and homework

---
+++ week_07/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+// <тип> <име_на_масив>[<размер_на_масива>];
+
+// [100,50,33,10]
+//  0 ,1, 2, 3   -> index
+
+// брой елементи = n
+// максимален индекс = n-1
+
+void fillArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "arr[" << i << "] = ";
+        cin >> arr[i];
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+void printArray(char arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i];
+    }
+
+    cout << endl;
+}
+
+int arrLength(char chArr[]) {
+    int length = 0;
+
+	while (chArr[length] != '\0') { 
+		++length;
+	}
+
+    return length;
+}
+
+const int SIZE = 100;
+
+// при обхождане на символен низ 
+// винаги while (charArr[i] != '/0')
+
+int main() {
+    // int n;
+    // cout << "Enter N: ";
+    // cin >> n;
+    
+    // int arr[n];
+    // fillArray(arr, n);
+    // printArray(arr, n);
+
+    // char word[] = {'f', 'm', 'i'};
+    // printArray(word, 3);
+
+    // char sentence[SIZE];
+    // cin.getline(sentence, SIZE);
+    // printArray(sentence, SIZE);
+
+
+    // обратно въвеждане
+    // int arr2[10];
+    // arr2[9] = 5;
+    // arr2[8] = 1;
+    // cout << arr2[9];
+    // cout << arr2[8];
+
+    // sizeof(arr) / sizeof(<arr_type>) = array length
+
+    char word[] = {'f', 'm', 'i'};
+    int days[] = {1,2,3,4,5};
+    cout << sizeof(word) << endl;
+    cout << sizeof(days) / sizeof(int) << endl;
+}
